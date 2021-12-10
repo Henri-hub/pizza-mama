bd5fa30fc38b5e342d55aa1d4e2c7b605b176ec3
Henri-hub <henrituaks@yahoo.com>
2021-12-10 16:57:59 +0100

    commit first

---
+++ pizza_mama/Controllers/ApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pizza_mama.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace pizza_mama.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : Controller //ControllerBase
+    {
+        DataContext dataContext;
+        public ApiController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+        
+        [HttpGet]
+        [Route("GetPizzas")]
+        public IActionResult GetPizzas()
+        {
+            // Récuperer les pizzas au format json depuis la base de données
+            var pizzas = dataContext.Pizzas.ToList();
+
+            return Json(pizzas);
+        }
+        
+    }
+}
